456da4200b7bbcb17d03c9db63f3104784987ff1
wastegas <wastegas@gmail.com>
2014-04-10 21:24:39 -1000

    fixed segfault

--- src/parse.c
+++ src/parse.c
@@ -9,11 +9,25 @@
 #include <editline/history.h>
 #endif
 
-/* Declare a static buffer for user input of maximum size 2048 */
-//static char input[2048]; --- using editline functions now
 
 int main(int argc, char** argv)
 {
+    /* Create some Parsers */
+    mpc_parser_t* Number    = mpc_new("number");
+    mpc_parser_t* Operator  = mpc_new("operator");
+    mpc_parser_t* Expr      = mpc_new("expr");
+    mpc_parser_t* Lispy     = mpc_new("lispy");
+
+    /* Define them with the following Language */
+    mpca_lang(MPC_LANG_DEFAULT,
+        "                                                       \
+          number    : /-?[0-9]+/ ;                              \
+          operator  : '+' | '-' | '*' | '/' ;                   \
+          expr      : <number> | '(' <operator> <expr>+ ')' ;   \
+          lispy     : /^/ <operator> <expr>+ /$/ ;              \
+        ",
+    Number, Operator, Expr, Lispy);
+
     /* Print version and exit information */
     puts("Lispy Version 0.0.0.0.1");
     puts("Press Ctrl-c to Exit\n");
@@ -22,19 +36,27 @@ int main(int argc, char** argv)
     while (1)
     {
         /* output our prompt */
-        //fputs("lispy> ", stdout); -- using editline functions now
         char* input = readline("lispy> ");
 
-        /* read a line of user input of a maximum size 2048 */
-        //fgets(input, 2048, stdin); -- using editline functions now
         add_history(input);
 
-        /* echo input back to user */
-        printf("\'%s\'? What do you mean?\n", input);
+        /* Attempt to Parse the user Input */
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+            /* On Success Print the AST */
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            /* otherwise Print the error */
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         /* Free retrieved input */
         free(input);    // housekeeping for editline
-    }
+   }
+    /* Undefine and delete our Parsers */
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
     return 0;
 }
