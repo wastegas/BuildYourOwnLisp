6b04db8d079f772eac3495546fc087ed724bcd86
wastegas <wastegas@gmail.com>
2014-04-20 17:38:03 -1000

    fixed some bugs

--- src/s_expressions.c
+++ src/s_expressions.c
@@ -269,7 +269,8 @@ int main(int argc, char** argv)
 {
     /* Create some Parsers */
     mpc_parser_t* Number    = mpc_new("number");
-    mpc_parser_t* Operator  = mpc_new("operator");
+    mpc_parser_t* Symbol    = mpc_new("symbol");
+    mpc_parser_t* Sexpr     = mpc_new("sexpr");
     mpc_parser_t* Expr      = mpc_new("expr");
     mpc_parser_t* Lispy     = mpc_new("lispy");
 
@@ -277,11 +278,12 @@ int main(int argc, char** argv)
     mpca_lang(MPC_LANG_DEFAULT,
         "                                                       \
           number    : /-?[0-9]+/ ;                              \
-          operator  : '+' | '-' | '*' | '/' ;                   \
-          expr      : <number> | '(' <operator> <expr>+ ')' ;   \
-          lispy     : /^/ <operator> <expr>+ /$/ ;              \
+          symbol    : '+' | '-' | '*' | '/' ;                   \
+          sexpr     : '(' <expr>* ')';                          \
+          expr      : <number> | <symbol> | <sexpr> ;           \
+          lispy     : /^/ <expr>* /$/ ;                         \
         ",
-    Number, Operator, Expr, Lispy);
+    Number, Symbol, Sexpr, Expr, Lispy);
 
     /* Print version and exit information */
     puts("Lispy Version 0.0.0.0.5");
@@ -312,7 +314,7 @@ int main(int argc, char** argv)
         free(input);    // housekeeping for editline
    }
     /* Undefine and delete our Parsers */
-    mpc_cleanup(4, Number, Operator, Expr, Lispy);
+    mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lispy);
 
     return 0;
 }
