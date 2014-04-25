7b65395ce6d56843249abffd85ee71cc5ad3f64a
wastegas <wastegas@gmail.com>
2014-04-24 21:12:44 -1000

    continue adding new functions

--- src/q_expressions.c
+++ src/q_expressions.c
@@ -15,23 +15,20 @@ enum {
     LVAL_ERR,
     LVAL_NUM,
     LVAL_SYM,
-    LVAL_SEXPR
+    LVAL_SEXPR,
+    LVAL_QEXPR
 };
 
-/* declare new lval struct */
 typedef struct lval {
     int type;
     long num;
-    /* Error and symbol types have some string data */
     char* err;
     char* sym;
 
-    /* Count and Pointer to a list of "lval" */
     int count;
     struct lval** cell;
 } lval;
 
-/* Construct a pointer to a new Number lval */
 lval* lval_num(long x) {
     lval* v = malloc(sizeof(lval));
     v->type = LVAL_NUM;
@@ -39,8 +36,6 @@ lval* lval_num(long x) {
     return v;
 }
 
-
-/* construct a pointer to a new error type lval */
 lval* lval_err(char* m) {
     lval* v = malloc(sizeof(lval));
     v->type = LVAL_ERR;
@@ -49,7 +44,6 @@ lval* lval_err(char* m) {
     return v;
 }
 
-/* construct a pointer to a new symbol lval */
 lval* lval_sym(char * s) {
     lval* v = malloc(sizeof(lval));
     v->type = LVAL_SYM;
@@ -58,10 +52,17 @@ lval* lval_sym(char * s) {
     return v;
 }
 
-/* a pointer to a new empty Sexpr lval */
 lval* lval_sexpr(void) {
     lval* v = malloc(sizeof(lval));
     v->type = LVAL_SEXPR;
+    v->count = 0;
+    v->cell = NULL;
+    return v;
+}
+
+lval* lval_qexpr(void) {
+    lval* v = malloc(sizeof(lval));
+    v->type = LVAL_QEXPR;
     v->count = 0;
     v->cell = NULL;
     return v;
@@ -78,6 +79,7 @@ void lval_del(lval* v) {
         case LVAL_SYM: free(v->sym); break;
         
         /*if Sexpr then delete all elements inside */
+        case LVAL_QEXPR:
         case LVAL_SEXPR:
             for (int i = 0; i < v->count ; i++) {
                 lval_del(v->cell[i]);
@@ -151,6 +153,7 @@ void lval_println(lval* v) {
     putchar('\n');
 }
 
+#define LASSERT(args, cond, err) if (!(cond)) { lval_del(args); return lval_err(err); }
 
 lval* builtin_op(lval* a, char* op) {
 
