6f616c1687e5336109932c5f00bca7051bf23fe1
wastegas <wastegas@gmail.com>
2014-05-08 15:22:53 -1000

    add new builtin_def function

--- src/variables.c
+++ src/variables.c
@@ -381,6 +381,29 @@ lval *builtin_sub(lenv* e, lval* a) { return builtin_op(e, a, "-"); }
 lval *builtin_mul(lenv* e, lval* a) { return builtin_op(e, a, "*"); }
 lbal *builtin_div(lenv* e, lval* a) { return builtin_op(e, a, "/"); }
 
+lval *builtin_def(lenv* e, lval* a) {
+    LASSERT_TYPE("def", a, 0, LVAL_QEXPR);
+
+    lval* syms = a->cell[0];
+
+    for (int i = 0; i < syms->count; i++) {
+        LASSERT(a, (syms->cell[i]->type == LVAL_SYM),
+                "Function 'def' cannot define non-symbol. Got %s, Expected %s.",
+                ltype_name(syms->cell[i]->type), ltype_name(LVAL_SYM));
+    }
+
+    LASSERT(a, (syms->count == a->count - 1),
+            "Function 'def' passed too many arguments for symbols. Got %i, Expected %i.",
+            syms->count, a->count-1);
+
+    for (int i = 0; i < syms->count; i++) {
+        lenv_put(e, syms->cell[i], a->cell[i+1]);
+    }
+
+    lval_del(a);
+    return lval_sexpr();
+}
+
 lval* builtin(lval* a, char* func) {
     if (strcmp("list", func) == 0) { return builtin_list(a); }
     if (strcmp("head", func) == 0) { return builtin_head(a); }
