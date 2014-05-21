32c2278f72e7d43bb8e835bf4b16ff494e975c1d
wastegas <wastegas@gmail.com>
2014-05-20 20:20:31 -1000

    continue adding new builtin functions

--- src/conditionals.c
+++ src/conditionals.c
@@ -515,6 +515,35 @@ lval *builtin_put(lenv* e, lval* a) {
     return builtin_var(e, a, "=");
 }
 
+lval* builtin_ord(lenv* e, lval* a, char* op) {
+    LASSERT_NUM(op, a, 2);
+    LASSERT_TYPE(op, a, 0, LVAL_NUM);
+    LASSERT_TYPE(op, a, 1, LVAL_NUM);
+    
+    int r;
+    if(strcmp(op, ">") == 0) { r = (a->cell[0]->num > a->cell[1]->num); }
+    if(strcmp(op, "<") == 0) { r = (a->cell[0]->num < a->cell[1]->num); }
+    if(strcmp(op, ">=")== 0) { r = (a->cell[0]->num >=a->cell[1]->num); }
+    if(strcmp(op, "<=")== 0) { r = (a->cell[0]->num <=a->cell[1]->num); }
+    lval_del(a);
+    return lval_num(r);
+}
+
+lval* builtin_gt(lenv* e, lval* a) { return builtin_ord(e, a, ">"); }
+lval* builtin_lt(lenv* e, lval* a) { return builtin_ord(e, a, "<"); }
+lval* builtin_ge(lenv* e, lval* a) { return builtin_ord(e, a, ">="); }
+lval* builtin_le(lenv* e, lval* a) } return builtin_ord(e, a, "<="); }
+
+lval* builtin_cmp(lenv* e, lval* a, char* op) {
+    LASSERT_NUM(op, a, 2);
+    int r;
+    if (strcmp(op, "==") == 0) { r = lval_eq(a->cell[0], a->cell[1]); }
+    if (strcmp(op, "!=") == 0) { r = !lval_eq(a->cell[0], a->cell[1]); }
+    lval_del(a);
+    return lval_num(r);
+}
+       
+
 void lenv_add_builtin(lenv* e, char* name, lbuiltin func) {
     lval* k = lval_sym(name);
     lval* v = lval_builtin(func);
