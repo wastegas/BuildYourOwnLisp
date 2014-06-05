8be8ed7fd65dd5187f2b6db2f22325ff7361e1c7
wastegas <wastegas@gmail.com>
2014-06-04 15:54:52 -1000

    new builtin_error function

--- src/strings.c
+++ src/strings.c
@@ -638,6 +638,26 @@ lval* builting_load(lenv* e, lval* a) {
     }
 }
 
+lval* builtin_print(lenv* e, lval* a) {
+    for (int i = 0; i < a->count; i++) {
+        lval_print(a->cell[i]); 
+        putchar(' ');
+    }
+    putchar('\n');
+    lval_del(a);
+
+    return lval_sexpr();
+}
+
+lval* builtin_error(lenv* e, lval* a) {
+    LASSERT_NUM("error", a, 1);
+    LASSERT_TYPE("error", a, 0, LVAL_STR);
+
+    lval* err = lval_err(a->cell[0]->str);
+    lval_del(a);
+    return err;
+}
+
 void lenv_add_builtin(lenv* e, char* name, lbuiltin func) {
     lval* k = lval_sym(name);
     lval* v = lval_fun(func);
