143ef14f56fe618f5f81fe6f9f5b5e2f67d5ecf6
wastegas <wastegas@gmail.com>
2014-06-06 20:16:11 -1000

    modify main to make use of new functions

--- src/strings.c
+++ src/strings.c
@@ -864,39 +864,51 @@ int main(int argc, char** argv)
         ",
     Number, Symbol, String, Comment, Sexpr, Qexpr, Expr, Lispy);
 
-    puts("Lispy Version 0.0.0.1.0");
-    puts("Press Ctrl-c to Exit\n");
-
     lenv* e = lenv_new();
     lenv_add_builtins(e);
 
-    while (1)
-    {
-        char* input = readline("lispy> ");
-        add_history(input);
+    if (argc == 1) {
+        puts("Lispy Version 0.0.0.1.0");
+        puts("Press Ctrl-c to Exit\n");
 
-        mpc_result_t r;
-        if (mpc_parse("<stdin>", input, Lispy, &r)) {
- 
-            lval* x = lval_eval(e, lval_read(r.output));
-            lval_println(x);
+        
+        while (1)
+        {
+            char* input = readline("lispy> ");
+            add_history(input);
+
+            mpc_result_t r;
+            if (mpc_parse("<stdin>", input, Lispy, &r)) {
+     
+                lval* x = lval_eval(e, lval_read(r.output));
+                lval_println(x);
+                lval_del(x);
+
+                mpc_ast_delete(r.output);
+            } else {
+                /* otherwise Print the error */
+                mpc_err_print(r.error);
+                mpc_err_delete(r.error);
+            }
+
+            /* Free retrieved input */
+            free(input);    // housekeeping for editline
+       }
+    }
+
+    if (argc >= 2) {
+        for (int i = 1; i < argc; i++) {
+            lval* args = lval_add(lval_sexpr(), lval_str(argv[i]));
+            lval* x = builtin_load(e, args);
+            if (x->type == LVAL_ERR) { lval_println(x); }
             lval_del(x);
-
-            mpc_ast_delete(r.output);
-        } else {
-            /* otherwise Print the error */
-            mpc_err_print(r.error);
-            mpc_err_delete(r.error);
         }
-
-        /* Free retrieved input */
-        free(input);    // housekeeping for editline
-   }
+    }
 
     lenv_del(e);
 
     /* Undefine and delete our Parsers */
-    mpc_cleanup(6, Number, Symbol, Sexpr, Qexpr, Expr, Lispy);
+    mpc_cleanup(8, Number, Symbol, String, Comment, Sexpr, Qexpr, Expr, Lispy);
 
     return 0;
 }
